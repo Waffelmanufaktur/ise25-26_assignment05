a495b49c2ac11f933fbe76032841017cef21ea21
jsurm <joachimsurm@gmail.com>
2025-11-16 20:18:37 +0100

    FIXED BUG PosController.java
    Old Version of filtering by name via getName was implemented -> didn't work, therefore updated the version. Problem was the @RequestParam(filter = name) had to be changed to @RequestParam(name).

--- api/src/main/java/de/seuhd/campuscoffee/api/controller/PosController.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/controller/PosController.java
@@ -45,7 +45,7 @@ public class PosController {
     // TODO: Implement a new GET endpoint that supports filtering POS by name, e.g., /filter?name=Schmelzpunkt
     @GetMapping("/filter")
     public ResponseEntity<PosDto> getByName(
-            @RequestParam(filter = "name") String name) {
+            @RequestParam("name") String name) {
         return ResponseEntity.ok(
                 posDtoMapper.fromDomain(posService.getByName(name))
         );
